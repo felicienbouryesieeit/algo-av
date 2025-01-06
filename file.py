c5fb4dc86479f804f66c7e34e377c34527372513
Romain <romain.deschaseaux@outlook.fr>
2025-01-06 11:12:13 +0100

    Ajouts des Commentaires

--- file.py
+++ file.py
@@ -1,12 +1,16 @@
+# Création d'une nouvelle file
 def nouvelle_file():
     return list()
 
+# Ajout d'un élément à la fin de la file
 def enfiler(f, e):
     f.append(e)
 
-
+# Retrait du premier élément de la file
+# Assurez-vous que la file n'est pas vide avant d'appeler cette fonction
 def defiler(f):
     return f.pop(0)
 
+# Vérifie si la file est vide
 def file(f):
     return len(f) == 0
